aaaf929975666c0d8e3ec443cd4531d6ef3a1c4f
Helen Yau <helenyau13@gmail.com>
2017-04-06 14:16:15 -0700

    install twit and try to get the twitter bot working

---
+++ database/database.js
@@ -0,0 +1,24 @@
+const pgp = require('pg-promise')()
+const fs = require('fs')
+
+if(fs.existsSync('.env')) {
+  require('dotenv').config()
+}
+const connectionString = process.env.DATABASE_URL;
+const db = pgp(connectionString)
+
+
+const Twitter = {
+  getAllTweets: () => {
+    const psql = `SELECT * FROM twitbot`
+    return db.any(psql)
+  },
+  addTweets: (tweet) => {
+    const psql = `INSERT INTO twitbot(tweets) VALUES($1) RETURNING *`
+    return db.any(psql, tweet)
+  }
+}
+
+module.exports = {
+  Twitter
+}
