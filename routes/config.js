5aa9ad4f75d136072c22518a81cbf4153692f47e
helenyau0 <helenyau13@gmail.com>
2017-04-07 10:10:18 -0700

    Update config.js

--- routes/config.js
+++ routes/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key: 'q6oXlVcdqCqGSkfzZNbDeD5Qb'
-  , consumer_secret: 'u2sWh8ujmjU7CD9uCaGjTvgLzFPw6QortgH2yLreNcEPK5OYVV'
-  , access_token: '849038579875553281-XlwLBIMGD4zn4pSw5ECJBYEurdDsWbk'
-  , access_token_secret: 'GSgECYQ4VlFeo0nFe5kYEe4ulukdpuXKHvdL3V3v2oDcP'
+  consumer_key: '...'
+  , consumer_secret: '...'
+  , access_token: '...'
+  , access_token_secret: '...'
 }
